29a7fb49bf1cca7c403458d5d9cd9700320d718d
Chris-26-dev <christian.m.panerio@gmail.com>
2025-12-04 16:34:01 +0800

    Edit todo form

--- src/components/todo-form.tsx
+++ src/components/todo-form.tsx
@@ -8,6 +8,7 @@ import z from "zod"
 import { db } from "@/db"
 import { todos } from "@/db/schema"
 import { redirect } from "@tanstack/react-router";
+import { eq } from "drizzle-orm"
 
 const addTodo = createServerFn({ method: "POST" })
     .inputValidator(z.object({ name: z.string().min(1) }))
@@ -17,10 +18,24 @@ const addTodo = createServerFn({ method: "POST" })
         throw redirect({ to: "/" })
     })
 
-export function TodoForm() {
+const updateTodo = createServerFn({ method: "POST" })
+    .inputValidator(z.object({ id: z.string().min(1), name: z.string().min(1) }))
+    .handler(async ({ data }) => {
+        await db.update(todos).set(data).where(eq(todos.id, data.id))
+
+        throw redirect({ to: "/" })
+    })
+
+export function TodoForm({ todo }: {
+    todo?: {
+        name: string
+        id: string
+    }
+}) {
     const nameRef = useRef<HTMLInputElement>(null)
     const [isLoading, setIsLoading] = useState(false)
     const addTodoFn = useServerFn(addTodo)
+    const updateTodoFn = useServerFn(updateTodo)
 
     async function handleSubmit(e: FormEvent) {
         e.preventDefault()
@@ -28,9 +43,11 @@ export function TodoForm() {
         if (!name) return
 
         setIsLoading(true)
-        
-        await addTodoFn({ data: { name } })
-
+        if (todo == null) {
+            await addTodoFn({ data: { name } })
+        } else {
+            await updateTodoFn({ data: { name, id: todo.id } })
+        }
         setIsLoading(false)
     }
 
@@ -42,10 +59,17 @@ export function TodoForm() {
                 placeholder="Enter your todo..."
                 className="flex-1"
                 aria-label="Name"
+                defaultValue={todo?.name}
             />
             <Button type="submit" disabled={isLoading}>
                 <LoadingSwap isLoading={isLoading} className="flex gap-2 items-center">
-                    <PlusIcon /> Add
+                    {todo == null ? (
+                        <>
+                            <PlusIcon /> Add
+                        </>
+                    ) : (
+                        "Update"
+                    )}
                 </LoadingSwap>
             </Button>
         </form>
